fc9fc02ac41ee424523559366c92b39c8570a3a3
Shivam Khandare <shivamsk3019@gmail.com>
2025-07-19 10:15:02 +0530

    Message routing

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,5 +1,6 @@
 import express from "express"
 import authRoutes from "./routes/auth.route.js"
+import messageRoutes from "./routes/message.route.js"
 import dotenv from "dotenv"
 import cookieParser from "cookie-parser"
 import { connectDB } from "./lib/db.js";
@@ -9,7 +10,9 @@ const app = express();
 
 app.use(express.json())
 app.use(cookieParser())
+
 app.use('/api/auth', authRoutes)
+app.use('/api/message', messageRoutes)
 
 const PORT = process.env.PORT || 5001;
 
