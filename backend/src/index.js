d2a0dc1d06372bd0ac5f1aebd6d9073646a3b47f
Shivam Khandare <shivamsk3019@gmail.com>
2025-07-20 23:51:44 +0530

    Signup and Login Page

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express"
 import authRoutes from "./routes/auth.route.js"
 import messageRoutes from "./routes/message.route.js"
 import dotenv from "dotenv"
+import cors from "cors"
 import cookieParser from "cookie-parser"
 import { connectDB } from "./lib/db.js";
 
@@ -10,6 +11,11 @@ const app = express();
 
 app.use(express.json())
 app.use(cookieParser())
+app.use(cors({
+    origin: "http://localhost:5173", 
+    credentials: true
+
+}))
 
 app.use('/api/auth', authRoutes)
 app.use('/api/message', messageRoutes)
