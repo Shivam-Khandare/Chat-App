bc49f7367626c436cbcb8b18f8e4f1551a28e13d
Shivam Khandare <shivamsk3019@gmail.com>
2025-07-23 02:51:09 +0530

    Final commit

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,9 +6,12 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 import { connectDB } from "./lib/db.js";
 import { app, server } from "./lib/socket.js"
+import path from "path"
 
 dotenv.config()
 
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: '50mb' }))
 app.use(cookieParser())
@@ -21,7 +24,13 @@ app.use(cors({
 app.use('/api/auth', authRoutes)
 app.use('/api/messages', messageRoutes)
 
-const PORT = process.env.PORT || 5001;
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("*", (req, res)=>{
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
+    })
+}
 
 server.listen(PORT, ()=>{
     console.log(`Server is running on PORT: ${PORT}`)
